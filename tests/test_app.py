e73c948113f323f0e59e485884e60a2cdb23fdf5
NiteeshKanna <mniteeshKanna@gmail.com>
2026-07-02 12:23:07 +0530

    modified test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
-from src.app import add, subtract
+from src.app import add, sub
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(5, 3) == 2
+    assert sub(5, 3) == 2
